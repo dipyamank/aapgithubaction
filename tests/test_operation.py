85fff3ad8182ab7ef2c466731d1f2be2f382dcd7
dipyamank <dipyamank@gmail.com>
2025-05-17 15:43:41 +0100

    Unit test cases updated

---
+++ tests/test_operation.py
@@ -0,0 +1,12 @@
+
+from src.math_operations import add,sub
+
+def test_add():
+    assert add(5,2)==7
+    assert add(5,10)==15
+
+    
+def test_sub():
+    assert add(5,2)==2
+    assert sub(5,10)==-5
+
